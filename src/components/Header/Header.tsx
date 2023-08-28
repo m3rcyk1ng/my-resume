eb504796317ad0fce76072c0c4f59070d43e335c
Vova Gridnev <gridnevmami@gmail.com>
2023-08-28 12:19:24 +0300

    fix: fix anim & def value for date input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -40,7 +40,7 @@ export const Header: FunctionComponent<any> = ({ timeline }) => {
 
   useLayoutEffect(() => {
     if (isLangChange) return;
-    timeline.from(nameBlockRef1, 3, {
+    timeline.from(nameBlockRef1, 2, {
       y: -100,
       ease: Power3.easeOut,
       opacity: 0,
@@ -49,27 +49,27 @@ export const Header: FunctionComponent<any> = ({ timeline }) => {
     timeline
       .fromTo(
         btnContainerRef,
-        2.5,
+        2,
         { y: 50, opacity: 0, scale: 0.9 },
         { y: 0, opacity: 1, scale: 1 },
-        4,
+        3,
       )
-      .fromTo(resumeBtnRef, 1, { x: '-2500', opacity: 0 }, { x: 0, opacity: 1 }, 3.5)
-      .fromTo(contactBtnRef, 1, { x: '2500', opacity: 0 }, { x: 0, opacity: 1 }, 4);
+      .fromTo(resumeBtnRef, 0.75, { x: '-2500', opacity: 0 }, { x: 0, opacity: 1 }, 2.5)
+      .fromTo(contactBtnRef, 0.75, { x: '2500', opacity: 0 }, { x: 0, opacity: 1 }, 3);
 
     timeline
-      .from(profilePhotoRef, 5, { y: 1500, ease: Power3.easeInOut, opacity: 0 }, 0)
-      .from(nameBlockRef2, 1.2, { y: 1200, ease: Power3.easeOut, opacity: 0 }, 1.5)
-      .from(nameBlockRef2, 2, { scale: 1.6, ease: Power3.easeOut }, 1.5)
-      .from(nameBlockRef3, 1.2, { y: 1200, ease: Power3.easeOut, opacity: 0 }, 2)
-      .from(nameBlockRef3, 2, { scale: 1.6, ease: Power3.easeOut }, 2)
-      .from(languageRef, 1.5, { opacity: 0, scale: 0.7 }, 6);
+      .from(profilePhotoRef, 4, { y: 1500, ease: Power3.easeInOut, opacity: 0 }, 0)
+      .from(nameBlockRef2, 1, { y: 1200, ease: Power3.easeOut, opacity: 0 }, 1.25)
+      .from(nameBlockRef2, 1.5, { scale: 1.6, ease: Power3.easeOut }, 1)
+      .from(nameBlockRef3, 1, { y: 1200, ease: Power3.easeOut, opacity: 0 }, 1.75)
+      .from(nameBlockRef3, 1.5, { scale: 1.6, ease: Power3.easeOut }, 1.5)
+      .from(languageRef, 1, { opacity: 0, scale: 0.7 }, 4);
 
     timeline.from(
       [scrollDownRef, socialsRef],
       3,
       { y: 100, ease: Power3.easeOut, opacity: 0 },
-      6,
+      5,
     );
   });
 
